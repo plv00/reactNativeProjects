c9ecbbbbdfa343a655ab2e63823e595f9a84b61a
Peter <plv829@gmail.com>
2018-03-20 10:57:18 -0400

    Resuable form, able to edit and create

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,6 +3,8 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
+
 
 const RouterComponent = () => {
   return(
@@ -11,17 +13,20 @@ const RouterComponent = () => {
         <Scene key="auth">
           <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
+
         <Scene key="main">
-          <Scene 
-            rightTitle="Add"
-            onRight={() => Actions.employeeCreate()}
-            key="employeeList" 
-            component={EmployeeList} 
-            title="Employees"
-            initial  
-            />
-        </Scene>
+        <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add"
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          initial
+        />
         <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
+      </Scene>
+        
       </Scene>
     </Router>
   );
