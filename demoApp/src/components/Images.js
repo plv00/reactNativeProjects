8fc324c53892617e0b8d4d7613020e645c6314b0
Peter <plv829@gmail.com>
2018-04-03 14:04:04 -0400

    testing images

---
+++ demoApp/src/components/Images.js
@@ -0,0 +1,14 @@
+export const Images = {
+  
+  a1: require('../images/1.png'),
+  a2: require('../images/2.png'),
+  a3: require('../images/3.png'),
+  a4: require('../images/4.png'),
+  a5: require('../images/5.png'),
+  a6: require('../images/6.png'),
+  a7: require('../images/7.png'),
+  a8: require('../images/8.png'),
+  a9: require('../images/9.png')
+  
+}
+
--- demoApp/src/components/PokemonList.js
+++ demoApp/src/components/PokemonList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, Text, View, Image } from 'react-native';
 import data from './pokemons.json'
+import Images from './Images'
 
 class PokemonList extends Component {
   state={ pokemons: [] };
@@ -10,15 +11,32 @@ class PokemonList extends Component {
   }
 
   render() {
+    const img = Images;
+    console.log("images are: ",img)
+
     return(
       <View style={styles.container}>
         <FlatList
           data={this.state.pokemons}
+          
           renderItem={({item}) => 
             <View>
+              <Image
+                  source={{ uri: 'asset:/1.png'}}
+                style={{width: 50, height: 50, flex: 1}}
+              />
+              <Image
+                  source={require('../images/4.png')}
+                style={{width: 50, height: 50, flex: 1}}
+              />
+              {/* <Image
+                  source={img[item.id]}
+                style={{width: 50, height: 50, flex: 1}}
+              /> */}
               <Text style={styles.item}>{item.name}</Text>
             </View>
             }
+            keyExtractor={item => item.id}
         />
       </View>
     );
